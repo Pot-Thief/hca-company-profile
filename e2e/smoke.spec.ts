a8ce4d6da53b716dc08bd550260bf87052d46bd4
bagusyatma <bagusyatma1@gmail.com>
2026-08-30 04:22:01 +0700

    add end to end structure and responsive specs

--- e2e/smoke.spec.ts
+++
@@ -1,7 +0,0 @@
-// e2e/smoke.spec.ts
-import { expect, test } from '@playwright/test';
-
-test('home page responds', async ({ page }) => {
-  const response = await page.goto('/');
-  expect(response?.status()).toBe(200);
-});
